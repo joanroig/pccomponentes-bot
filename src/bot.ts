047fc1ed6cd8e6db5f4765683b13ac4c1f083a30
Joan Roig <12451419+joanroig@users.noreply.github.com>
2021-05-09 23:27:16 +0200

    Switching to puppeteer. Login fixed.

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,10 @@
 // Read values from the configuration file
-const config = require("../config.json");
+// const config = require("../config.json");
+import { Service } from "typedi";
+import * as config from "../config.json";
+import NotifyService from "./services/notify.service";
+import PurchaseService from "./services/purchase.service";
+
 const url = config.url;
 const maxPrice = config.maxPrice;
 const models = config.models;
@@ -9,11 +14,6 @@ const purchaseCombos = config.purchaseCombos;
 var request = require("request");
 const sanitizeHtml = require("sanitize-html");
 var html2json = require("html2json").html2json;
-
-import { Service } from "typedi";
-import NotifyService from "./services/notify.service";
-import PurchaseService from "./services/purchase.service";
-import Utils from "./utils";
 
 @Service()
 export default class Bot {
@@ -62,116 +62,105 @@ export default class Bot {
   }
 
   updateData() {
-    request(url, (err: any, res: any, body: any) => {
-      // Allow only a super restricted set of tags and attributes to clean the HTML
-      const clean = sanitizeHtml(body, {
-        allowedTags: ["article", "a"],
-        allowedAttributes: {
-          article: ["data-price", "data-name"],
-          a: ["href"],
-        },
-      });
-
-      // Convert the cleaned HTML output to JSON objects
-      const json = html2json(clean);
-      // console.log(json)
-
-      // List of items that match the requisites (each item is a string with price, name and URL)
-      var matches: string[] = [];
-
-      json?.child?.forEach((element: any) => {
-        if (element.attr) {
-          const price = element.attr["data-price"];
-          const name = element.attr["data-name"].map((v: string) =>
-            v.toLowerCase()
-          );
-
-          // Check if the price is in range
-          if (element.tag === "article" && price < maxPrice) {
-            // Check if any of the models are in the title of the product
-            if (models.some((el: string) => name.includes(el.toLowerCase()))) {
-              //  Build link, name and price of the product in a single string
-              let link =
-                "https://www.pccomponentes.com" +
-                element.child.find((link: any) => link.tag === "a").attr.href;
-
-              let nameText = "[" + name.join([" "]) + "](" + link + ")";
-              let priceText = "*" + price + " EUR*";
-              let match = priceText + "\n" + nameText;
-
-              // Check if it is an instant buy
-              purchaseCombos.forEach((combo: any) => {
-                if (
-                  !this.buying &&
-                  combo.price > price &&
-                  combo.model.every((v: string) =>
-                    name.includes(v.toLowerCase())
-                  )
-                ) {
-                  if (!this.purchased.includes(link)) {
-                    this.purchased.push(link);
-                    console.warn("\n*Nice price, start the purchase!!!*");
-                    console.warn(combo);
-                    this.buying = true;
-
-                    this.purchase
-                      .run(link, combo.price, 8000)
-                      .then((result) => {
-                        if (result) {
-                          console.warn(
-                            "\n------------\n*** PURCHASE FINISHED ***\n------------\n"
-                          );
-                          this.notify.notify(
-                            Utils.getDate() + "\n\nPURCHASED!\n\n",
-                            [match]
-                          );
-                          process.exit();
-                        } else {
-                          console.warn("Purchase failed...");
-                          this.buying = false;
-                        }
-                      });
-                  }
-                }
-              });
-
-              // console.log(match)
-              matches.push(match);
-            }
-          }
-        }
-      });
-
-      // Check if there is any new card - we use difference to only get the new cards: https://stackoverflow.com/questions/1187518/how-to-get-the-difference-between-two-arrays-in-javascript
-      const difference = matches.filter((x) => !this.previous.includes(x));
-
-      if (this.firstTime) {
-        // First update
-        console.log(
-          "\n" + Utils.getDate() + "\nBot started! Start tracking cards:"
-        );
-        console.log(difference);
-        this.notify.notify(
-          Utils.getDate() +
-            "\n\nBOT STARTED\nCommands: 'all', 'refresh'.\nFound cards:",
-          difference
-        );
-        this.firstTime = false;
-      } else if (difference.length > 0) {
-        // New cards found!
-        console.log("\n" + Utils.getDate() + "\nNew cards found!:");
-        console.log(difference);
-        this.notify.notify(
-          Utils.getDate() + "\n\nNEW CARDS FOUND!:",
-          difference
-        );
-      } else {
-        console.log("\n" + Utils.getDate() + "\nNo new cards found...");
-        // notify(Utils.getDate() + "\n\nno new cards...");
-      }
-
-      // Update previous
-      this.previous = matches;
-    });
+    // request(url, (err: any, res: any, body: any) => {
+    //   // Allow only a super restricted set of tags and attributes to clean the HTML
+    //   const clean = sanitizeHtml(body, {
+    //     allowedTags: ["article", "a"],
+    //     allowedAttributes: {
+    //       article: ["data-price", "data-name"],
+    //       a: ["href"],
+    //     },
+    //   });
+    //   // Convert the cleaned HTML output to JSON objects
+    //   const json = html2json(clean);
+    //   // console.log(json)
+    //   // List of items that match the requisites (each item is a string with price, name and URL)
+    //   var matches: string[] = [];
+    //   json?.child?.forEach((element: any) => {
+    //     if (element.attr) {
+    //       const price = element.attr["data-price"];
+    //       const name = element.attr["data-name"].map((v: string) =>
+    //         v.toLowerCase()
+    //       );
+    //       // Check if the price is in range
+    //       if (element.tag === "article" && price < maxPrice) {
+    //         // Check if any of the models are in the title of the product
+    //         if (models.some((el: string) => name.includes(el.toLowerCase()))) {
+    //           //  Build link, name and price of the product in a single string
+    //           let link =
+    //             "https://www.pccomponentes.com" +
+    //             element.child.find((link: any) => link.tag === "a").attr.href;
+    //           let nameText = "[" + name.join([" "]) + "](" + link + ")";
+    //           let priceText = "*" + price + " EUR*";
+    //           let match = priceText + "\n" + nameText;
+    //           // Check if it is an instant buy
+    //           purchaseCombos.forEach((combo: any) => {
+    //             if (
+    //               !this.buying &&
+    //               combo.price > price &&
+    //               combo.model.every((v: string) =>
+    //                 name.includes(v.toLowerCase())
+    //               )
+    //             ) {
+    //               if (!this.purchased.includes(link)) {
+    //                 this.purchased.push(link);
+    //                 console.warn("\n*Nice price, start the purchase!!!*");
+    //                 console.warn(combo);
+    //                 this.buying = true;
+    //                 this.purchase
+    //                   .run(link, combo.price, 8000)
+    //                   .then((result) => {
+    //                     if (result) {
+    //                       console.warn(
+    //                         "\n------------\n*** PURCHASE FINISHED ***\n------------\n"
+    //                       );
+    //                       this.notify.notify(
+    //                         Utils.getDate() + "\n\nPURCHASED!\n\n",
+    //                         [match]
+    //                       );
+    //                       process.exit();
+    //                     } else {
+    //                       console.warn("Purchase failed...");
+    //                       this.buying = false;
+    //                     }
+    //                   });
+    //               }
+    //             }
+    //           });
+    //           // console.log(match)
+    //           matches.push(match);
+    //         }
+    //       }
+    //     }
+    //   });
+    //   // Check if there is any new card - we use difference to only get the new cards: https://stackoverflow.com/questions/1187518/how-to-get-the-difference-between-two-arrays-in-javascript
+    //   const difference = matches.filter((x) => !this.previous.includes(x));
+    //   if (this.firstTime) {
+    //     // First update
+    //     console.log(
+    //       "\n" + Utils.getDate() + "\nBot started! Start tracking cards:"
+    //     );
+    //     console.log(difference);
+    //     this.notify.notify(
+    //       Utils.getDate() +
+    //         "\n\nBOT STARTED\nCommands: 'all', 'refresh'.\nFound cards:",
+    //       difference
+    //     );
+    //     this.firstTime = false;
+    //   } else if (difference.length > 0) {
+    //     // New cards found!
+    //     console.log("\n" + Utils.getDate() + "\nNew cards found!:");
+    //     console.log(difference);
+    //     this.notify.notify(
+    //       Utils.getDate() + "\n\nNEW CARDS FOUND!:",
+    //       difference
+    //     );
+    //   } else {
+    //     console.log("\n" + Utils.getDate() + "\nNo new cards found...");
+    //     // notify(Utils.getDate() + "\n\nno new cards...");
+    //   }
+    //   // Update previous
+    //   this.previous = matches;
+    // });
   }
 }
