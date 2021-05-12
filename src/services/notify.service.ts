c45cbcd4923e30d225e2e18a159c88a41b040448
Joan Roig <joanroigsegura@gmail.com>
2021-05-13 00:57:38 +0200

    Automatic save of the CHAT_ID in the .env file. General improvements on Telegram notifications and commands.

--- src/services/notify.service.ts
+++ src/services/notify.service.ts
@@ -1,12 +1,14 @@
+import axios from "axios";
 import { Subject } from "rxjs";
 import { Telegraf } from "telegraf";
 import { Service } from "typedi";
 import Log from "../utils/log";
+var fs = require("fs");
 
 @Service()
 export default class NotifyService {
   private telegraf!: Telegraf;
-  public requestUpdate!: Subject<boolean>;
+  public requestUpdate = new Subject<boolean>();
   private sendNotifications = false;
 
   constructor() {}
@@ -15,10 +17,20 @@ export default class NotifyService {
     return this.requestUpdate.asObservable();
   }
 
-  startTelegram() {
+  async startTelegram() {
     if (process.env.BOT_TOKEN) {
+      // Remove all commands sent while the bot was offline
+      try {
+        await axios.get(
+          "https://api.telegram.org/bot" +
+            process.env.BOT_TOKEN +
+            "/getUpdates?offset=-1"
+        );
+      } catch (error) {
+        Log.error("Error while cleaning offline commands: " + error);
+      }
+
       this.telegraf = new Telegraf(process.env.BOT_TOKEN);
-      this.requestUpdate = new Subject<boolean>();
     } else {
       Log.critical(
         "Please, set the BOT_TOKEN in the .env file (explained in the README.md)"
@@ -30,41 +42,60 @@ export default class NotifyService {
 
     // Telegram bot setup
     telegraf.start((ctx: any) => {
-      Log.important(
-        "Put this CHAT_ID in the .env file, then restart the bot: " +
-          ctx.chat.id
-      );
-      return ctx.reply(
-        "Put this CHAT_ID in the .env file, then restart the bot: " +
-          ctx.chat.id
-      );
+      this.saveChatId(ctx);
+      Log.breakline();
+      Log.important("Telegram: Start command received.");
     });
 
     // Method to get chat id: https://github.com/telegraf/telegraf/issues/204
-    telegraf.hears(["Getid", "getid", "id", "Id"], (ctx: any) => {
-      Log.important(
-        "Put this CHAT_ID in the .env file, then restart the bot: " +
-          ctx.chat.id
-      );
-      return ctx.reply(
-        "Put this CHAT_ID in the .env file, then restart the bot: " +
-          ctx.chat.id
-      );
+    telegraf.hears(["/id", "Getid", "getid", "id", "Id"], (ctx: any) => {
+      let message =
+        "Put this line in the .env file, then restart the bot: " +
+        "\nCHAT_ID=" +
+        ctx.chat.id;
+      ctx.reply(message);
+      Log.breakline();
+      Log.important("Telegram: Id command received.");
+      Log.important(message);
+      Log.breakline();
     });
 
     // Commands
-    telegraf.hears(["Hi", "hi", "Hello", "hello"], (ctx: any) => {
-      this.notify("The bot is running.");
-      Log.important("Telegram: Greeting command received.");
-    });
+    telegraf.hears(
+      ["/hi", "/hello", "Hi", "hi", "Hello", "hello"],
+      (ctx: any) => {
+        ctx.reply("The bot is running.");
+        Log.breakline();
+        Log.important("Telegram: Greeting command received.");
+        Log.breakline();
+      }
+    );
 
-    telegraf.hears(["Update", "update", "Refresh", "refresh"], (ctx: any) => {
-      this.requestUpdate.next(true);
-      this.notify("Data refresh requested.");
-      Log.important("Telegram: Data refresh command received.");
+    telegraf.hears(
+      ["/update", "/refresh", "Update", "update", "Refresh", "refresh"],
+      (ctx: any) => {
+        this.requestUpdate.next(true);
+        ctx.reply("Data refresh requested.");
+        Log.breakline();
+        Log.important("Telegram: Data refresh command received.");
+        Log.breakline();
+      }
+    );
+
+    telegraf.hears(["/kill"], (ctx: any) => {
+      ctx.reply("Shutting down in 5 seconds...");
+      Log.breakline();
+      Log.important(
+        "Telegram: Quit command received, shutting down in 5 seconds..."
+      );
+      Log.breakline();
+      setTimeout(() => {
+        process.exit(0);
+      }, 5000);
     });
 
     telegraf.launch().catch((error) => {
+      Log.breakline();
       Log.critical("Telegram error: " + error);
       process.exit(1);
     });
@@ -80,17 +111,62 @@ export default class NotifyService {
         if (results) {
           sendMessage += results?.join("\n\n");
         }
-        this.telegraf.telegram.sendMessage(process.env.CHAT_ID!, sendMessage, {
+        this.telegraf.telegram.sendMessage(process.env.CHAT_ID, sendMessage, {
           disable_web_page_preview: true,
           parse_mode: "Markdown",
         });
       } else {
+        Log.breakline();
         Log.important(
-          "Send a message to the Telegram chat with this command to get the CHAT_ID:"
+          "Send a message to the Telegram chat with this command to get notifications:"
         );
-        Log.config("getid", false);
+        Log.config("/start", false);
         Log.breakline();
       }
     }
+  }
+
+  startMessage() {
+    this.notify(
+      "BOT RUNNING\nTelegram commands you can write here:\n\n/start: Set the CHAT_ID to receive alerts\n/id: Print the CHAT_ID\n/hello: Check if the bot is running\n/refresh: Force a refresh of all trackers\n/kill: Shutdown the bot."
+    );
+  }
+
+  private saveChatId(ctx: any) {
+    // Save the chat id in the .env file for the next sessions
+    fs.readFile(".env", "utf8", (err: any, data: any) => {
+      var formatted;
+      if (data.includes("CHAT_ID=")) {
+        // Replace the chat id line
+        formatted = data.replace(
+          /CHAT_ID=.*/g,
+          "CHAT_ID=" + ctx.chat.id
+        ) as string;
+      } else {
+        // Append the chat id line
+        formatted = data + "\nCHAT_ID=" + ctx.chat.id;
+      }
+
+      fs.writeFile(".env", formatted, "utf8", (err: any) => {
+        if (err) {
+          console.warn(err);
+          Log.breakline();
+          Log.critical("Error while saving the CHAT_ID automatically.");
+          Log.important(
+            "Put this line in the .env file, then restart the bot: " +
+              "\nCHAT_ID=" +
+              ctx.chat.id
+          );
+          Log.breakline();
+        } else {
+          // Set the chat id for this session
+          process.env.CHAT_ID = ctx.chat.id;
+          Log.breakline();
+          Log.important("CHAT_ID saved in the .env file: " + ctx.chat.id);
+          Log.breakline();
+          this.startMessage();
+        }
+      });
+    });
   }
 }
