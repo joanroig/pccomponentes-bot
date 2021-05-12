2dbf2c4dd4b4ac06baeab2558c668d17bf954b10
Joan Roig <12451419+joanroig@users.noreply.github.com>
2021-05-12 19:30:40 +0200

    Notify service enhancements.

--- src/services/notify.service.ts
+++ src/services/notify.service.ts
@@ -5,31 +5,50 @@ import Log from "../utils/log";
 
 @Service()
 export default class NotifyService {
-  private telegraf: Telegraf;
-  public requestUpdate: Subject<boolean>;
+  private telegraf!: Telegraf;
+  public requestUpdate!: Subject<boolean>;
   private sendNotifications = false;
 
-  constructor() {
-    this.telegraf = new Telegraf(process.env.BOT_TOKEN!);
-    this.requestUpdate = new Subject<boolean>();
-  }
+  constructor() {}
 
   getRequestUpdates() {
     return this.requestUpdate.asObservable();
   }
 
   startTelegram() {
+    if (process.env.BOT_TOKEN) {
+      this.telegraf = new Telegraf(process.env.BOT_TOKEN);
+      this.requestUpdate = new Subject<boolean>();
+    } else {
+      Log.critical(
+        "Please, set the BOT_TOKEN in the .env file (explained in the README.md)"
+      );
+      process.exit(0);
+    }
+
     const telegraf = this.telegraf;
 
     // Telegram bot setup
     telegraf.start((ctx: any) => {
-      console.warn("Put this CHAT_ID in the .env file:" + ctx.chat.id);
-      ctx.reply("Welcome! Put this CHAT_ID in the .env file:" + ctx.chat.id);
+      Log.important(
+        "Put this CHAT_ID in the .env file, then restart the bot: " +
+          ctx.chat.id
+      );
+      return ctx.reply(
+        "Put this CHAT_ID in the .env file, then restart the bot: " +
+          ctx.chat.id
+      );
     });
     // Method to get chat id: https://github.com/telegraf/telegraf/issues/204
-    telegraf.hears("getid", (ctx: any) => {
-      console.warn(ctx.chat.id);
-      return ctx.reply("Put this CHAT_ID in the .env file:" + ctx.chat.id);
+    telegraf.hears(["Getid", "getid", "id", "Id"], (ctx: any) => {
+      Log.important(
+        "Put this CHAT_ID in the .env file, then restart the bot: " +
+          ctx.chat.id
+      );
+      return ctx.reply(
+        "Put this CHAT_ID in the .env file, then restart the bot: " +
+          ctx.chat.id
+      );
     });
     telegraf.hears(["Hi", "hi", "Hello", "hello"], (ctx: any) => {
       this.notify("The bot is running.");
@@ -48,14 +67,21 @@ export default class NotifyService {
   // Send a message using the Telegram bot
   notify(message: string, results?: string[]) {
     if (this.sendNotifications) {
-      let sendMessage = "*\n\n" + message + "* \n\n";
-      if (results) {
-        sendMessage += results?.join("\n\n");
+      if (process.env.CHAT_ID) {
+        let sendMessage = "*\n\n" + message + "* \n\n";
+        if (results) {
+          sendMessage += results?.join("\n\n");
+        }
+        this.telegraf.telegram.sendMessage(process.env.CHAT_ID!, sendMessage, {
+          disable_web_page_preview: true,
+          parse_mode: "Markdown",
+        });
+      } else {
+        Log.important(
+          "Send a message to the Telegram chat with this command to get the CHAT_ID:"
+        );
+        Log.config("getid", false);
       }
-      this.telegraf.telegram.sendMessage(process.env.CHAT_ID!, sendMessage, {
-        disable_web_page_preview: true,
-        parse_mode: "Markdown",
-      });
     }
   }
 }
