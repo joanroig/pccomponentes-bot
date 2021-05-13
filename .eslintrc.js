798a3c90fd8c8aef33d81c7215e9457cf50807e2
Joan Roig <joanroigsegura@gmail.com>
2021-05-13 15:55:08 +0200

    Prettier added and used as a plugin in ESLint. Hook for pre-commit modified to format before committing, and automatic patch version increase.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'standard'
-  ],
+  extends: ["standard", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
   rules: {
-  }
-}
+    "prettier/prettier": "error",
+  },
+  plugins: ["prettier"],
+};
